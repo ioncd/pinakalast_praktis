bcd41f622567df9d90e8855474dd9f6d5e15f1c5
Rapidash001 <faldenrichards011@gmail.com>
2025-05-26 11:12:07 +0800

    okay na ung saken

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\employee;
 use Illuminate\Support\Fascades\DB;
 use Response;
 use Illuminate\Http\Request;
-use App\Models\employee;
 
 class employeecontroller extends Controller
 {
     public function index()
     {   
+        $employees = employee::get();
 
-        return view ('employee.index');
+        return view ('employee.index', compact('employees'));
     }
 
     public function create()
@@ -23,16 +24,16 @@ class employeecontroller extends Controller
     public function store(Request $request)
     {
     $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
         
     ]);
 
-    ::create($request->all());
+    employee::create($request->all());
     return view ('employee.create');
     }
 
